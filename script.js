beb67b80ffc01c47e168f953c90e6968d6f5872e
Nathan Mark <nathanmark306@gmail.com>
2023-10-18 17:56:53 +0100

    feat: canvas generator

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hii,it Nathan");
+const squareContainer = document.querySelector(".container");
+
+function createSquare(width, color) {
+  const div = document.createElement("div");
+  div.style.cssText = `width:${width}px;
+                        background-color:${color};
+                        aspect-ratio: 1 / 1;
+                        border: solid red 1px;
+                        
+                        
+                        
+  `;
+  return div;
+}
+
+for (let index = 0; index < 250; index++) {
+  const square = createSquare(60, "yellow");
+  squareContainer.appendChild(square);
+}
