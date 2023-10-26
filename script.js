54251e79b827c0a136586b79c67ddc1f39a1ece1
Nathan Mark <nathanmark306@gmail.com>
2023-10-26 11:01:05 +0100

    refactor: complete canvas resizing feature

--- script.js
+++ script.js
@@ -1,26 +1,61 @@
-const squareContainer = document.querySelector(".container");
 const slider = document.querySelector(".slider");
+const colorPicker = document.querySelector(".color_picker");
+const squareContainer = document.querySelector(".container");
+const showGridBtn = document.querySelector(".show_grid_btn");
 
-function changeColor(element, color = "red") {
+// Grid
+function toggleGrid() {
+  const pixelList = document.querySelectorAll(".pixel");
+  showGridBtn.classList.contains("show")
+    ? pixelList.forEach((e) => e.classList.add("grid"))
+    : pixelList.forEach((e) => e.classList.remove("grid"));
+
+  updateBtnText();
+}
+
+function toggleClass(e) {
+  e.target.classList.toggle("show");
+}
+
+// Grid Btn
+showGridBtn.addEventListener("click", toggleClass);
+showGridBtn.addEventListener("click", toggleGrid);
+
+function updateBtnText() {
+  showGridBtn.classList.contains("show")
+    ? (showGridBtn.textContent = "Hide Gird")
+    : (showGridBtn.textContent = "Show Gird");
+}
+
+// Color
+function changeColor(element, color = colorPicker.value) {
   element.target.style.backgroundColor = color;
 }
 
-function createSquare(width, color) {
+// Create pixels
+function createPixel(width, color = "white") {
   const div = document.createElement("div");
   div.classList = "pixel";
   div.style.cssText = `width:${width}px;
                         background-color:${color};
                         aspect-ratio: 1 / 1;
-                        border: solid red 1px;                    
-                        
+                        flex-grow:1                     
   `;
   div.addEventListener("mouseover", changeColor);
   return div;
 }
 
+// Pixel Sizing
+slider.addEventListener("input", function (e) {
+  clearCanvas();
+  generateCanvas(Number(e.target.value));
+  toggleGrid();
+});
+
+// Canvas
 function generateCanvas(size) {
-  for (let index = 0; index < 750; index++) {
-    const square = createSquare(size, "yellow");
+  for (let index = 0; index < 1000; index++) {
+    const square = createPixel(size);
     squareContainer.appendChild(square);
   }
 }
@@ -31,9 +66,3 @@ function clearCanvas() {
 }
 
 generateCanvas(slider.value);
-
-slider.addEventListener("input", function (e) {
-  clearCanvas();
-  generateCanvas(Number(e.target.value));
-  console.log(Number(e.target.value));
-});
